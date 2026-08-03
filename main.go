d402914d0b65d907144815f916b95d5df6359085
hinst <hinstance@yandex.ru>
2026-08-03 20:21:38 +0200

    Group tables by folder

--- main.go
+++ main.go
@@ -49,11 +49,16 @@ func (me *App) openFile(fileName string) {
 func (me *App) flushOngoingHeader() {
 	if checkContainsToc(me.ongoingHeader) {
 		sectionName := getSectionName(me.ongoingHeader)
-		if "" == sectionName {
+		if sectionName == "" {
 			panic("Cannot find section name in TOC header")
 		}
-		filename := "./data/" + sanitizeFilename(sectionName) + ".sql"
-		me.openFile(filename)
+		folder, filename := splitSectionName(sectionName)
+		if folder != "" {
+			AssertError(os.MkdirAll("./data/"+folder, 0755))
+			me.openFile("./data/" + folder + "/" + filename + ".sql")
+		} else {
+			me.openFile("./data/" + filename + ".sql")
+		}
 	}
 	for _, header := range me.ongoingHeader {
 		me.writeOutput(me.outputFile, header)
@@ -72,6 +77,20 @@ func (me *App) receiveLine(line string) {
 		me.flushOngoingHeader()
 		me.writeOutput(me.outputFile, line)
 	}
+}
+
+func splitSectionName(sectionName string) (folder string, rest string) {
+	i := 0
+	for i < len(sectionName) && ((sectionName[i] >= 'A' && sectionName[i] <= 'Z') || (sectionName[i] >= 'a' && sectionName[i] <= 'z')) {
+		i++
+	}
+	if i == 0 || i == len(sectionName) {
+		rest = sanitizeFilename(sectionName)
+	} else {
+		folder = sanitizeFilename(sectionName[:i])
+		rest = sanitizeFilename(sectionName[i:])
+	}
+	return
 }
 
 func main() {
