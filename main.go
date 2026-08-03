0bfb9cd97acd074c0f273105c7ff87894c044239
hinst <hinstance@yandex.ru>
2026-08-03 19:07:22 +0200

    Define command line argument: input file

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
+import (
+	"flag"
+	"log"
+)
+
 type App struct {
+	inputFile string
 }
 
 func (me *App) run() {
+	if me.inputFile == "" {
+		log.Fatal("Input file is not specified")
+	}
 }
 
 func main() {
+	flag.Parse()
 	var app = new(App)
+	app.inputFile = flag.Arg(0)
 	app.run()
 }
---
+++ run.bat
@@ -0,0 +1,1 @@
+go build && postgresql-split.exe
