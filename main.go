d1de2d8ac2e9f947c087dc747b0118d0b7ef21d3
hinst <hinstance@yandex.ru>
2026-08-03 19:42:26 +0200

    Refactoring work in progress. Adding assert

--- main.go
+++ main.go
@@ -5,25 +5,24 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strings"
 )
 
 type App struct {
 	inputFilePath  string
 	outputFilePath string
 	outputFile     *os.File
+	ongoingHeader  []string
 }
 
-const BUFFER_SIZE = 32 * 1024 * 1024 // 32 MB
+const BUFFER_SIZE = 32 * 1024 * 1024
 
 func (me *App) run() {
 	if me.inputFilePath == "" {
 		log.Fatal("Input file is not specified")
 	}
 
-	file, fileError := os.Open(me.inputFilePath)
-	if fileError != nil {
-		log.Fatalf("Failed to open input file: %v", fileError)
-	}
+	file := AssertResultError(os.Open(me.inputFilePath))
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
@@ -32,9 +31,19 @@ func (me *App) run() {
 		line := scanner.Text()
 		me.receiveLine(line)
 	}
-	if scannerError := scanner.Err(); scannerError != nil {
-		log.Fatalf("Error reading input file: %v", scannerError)
+	me.flushOngoingHeader()
+	AssertError(scanner.Err())
+}
+
+func (me *App) writeOutput(output *os.File, content string) {
+	AssertResultError(output.WriteString(content + "\n"))
+}
+
+func (me *App) flushOngoingHeader() {
+	for _, header := range me.ongoingHeader {
+		me.writeOutput(me.outputFile, header)
 	}
+	me.ongoingHeader = nil
 }
 
 func (me *App) receiveLine(line string) {
@@ -42,11 +51,17 @@ func (me *App) receiveLine(line string) {
 		me.outputFilePath = "./init.sql"
 	}
 	if nil == me.outputFile {
-		outputFile, err := os.Create(me.outputFilePath)
-		if err != nil {
-			log.Fatalf("Failed to open output file %s: %v", me.outputFilePath, err)
-		}
+		outputFile := AssertResultError(os.Create(me.outputFilePath))
 		me.outputFile = outputFile
+	}
+
+	if strings.HasPrefix(line, "-- ") {
+		me.ongoingHeader = append(me.ongoingHeader, line)
+	} else {
+		me.flushOngoingHeader()
+	}
+	if me.outputFile != nil {
+		me.writeOutput(me.outputFile, line)
 	}
 }
 
