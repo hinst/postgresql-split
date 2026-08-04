bbc135f34acac6e64023a1b11959d93deba360ce
hinst <hinstance@yandex.ru>
2026-08-04 19:01:48 +0200

    Adding build mode

--- main.go
+++ main.go
@@ -5,8 +5,22 @@ import (
 )
 
 func main() {
+	buildDir := flag.String("build", "", "Build the dump from split files in the specified folder")
 	flag.Parse()
-	var app = new(DumpSplitter)
-	app.inputFilePath = flag.Arg(0)
-	app.run()
+
+	if *buildDir != "" {
+		outputFile := flag.Arg(0)
+		if outputFile == "" {
+			outputFile = "dump.sql"
+		}
+		var builder = &DumpBuilder{
+			dataDirectory:  *buildDir,
+			outputFilePath: outputFile,
+		}
+		builder.run()
+	} else {
+		var app = new(DumpSplitter)
+		app.inputFilePath = flag.Arg(0)
+		app.run()
+	}
 }
